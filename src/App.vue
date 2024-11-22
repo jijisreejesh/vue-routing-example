2b35367e2482a8d7e8590118da580c4edbd8a03b
bijukb <bijukb7233@gmail.com>
2024-11-22 13:58:08 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink,RouterView } from 'vue-router';
+
+</script>
+
+<template>
+ 
+  <!-- RouterLink: Used for navigation between routes without reloading the page. -->
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink>
+  <RouterLink to="/contact">Contact</RouterLink> 
+
+  
+
+  <!-- Vue has a special component called the router-view that will handle the entire process for us. 
+   RouterView: A placeholder that renders the matched component for the current route. -->
+  <RouterView></RouterView>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useRouter } from 'vue-router';
+const router=useRouter();
+const goToContact=()=>{
+    router.push('/contact')
+}
+</script>
+
+<template>
+
+<div>About page</div>
+<button @click="goToContact">Got to contact</button>
+
+</template>
+
+<style>
+
+</style>
